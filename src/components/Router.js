d9dcb1087063baee4786f7b55599b894938c6a48
Ukjae Lee <79234473+Gryffindor0ne@users.noreply.github.com>
2022-09-15 14:08:48 +0900

    Navigation 설정 및 logout 구현

--- src/components/Router.js
+++ src/components/Router.js
@@ -1,14 +1,20 @@
 import React from "react";
 import { BrowserRouter as Router, Routes, Route } from "react-router-dom";
-import Home from "../routes/Home";
-import Auth from "../routes/Auth";
+import Home from "routes/Home";
+import Auth from "routes/Auth";
+import Navigation from "components/Navigation";
+import Profile from "routes/Profile";
 
 const AppRouter = ({ isLoggedIn }) => {
   return (
     <Router>
+      {isLoggedIn && <Navigation />}
       <Routes>
         {isLoggedIn ? (
-          <Route path="/" element={<Home />} />
+          <>
+            <Route path="/" element={<Home />} />
+            <Route path="/profile" element={<Profile />} />
+          </>
         ) : (
           <Route path="/" element={<Auth />} />
         )}
--- src/routes/Profile.js
+++ src/routes/Profile.js
@@ -1,5 +1,19 @@
 import React from "react";
+import { signOut } from "firebase/auth";
+import { useNavigate } from "react-router-dom";
 
-const Profile = () => <span>Profile</span>;
+import { authService } from "fbase";
 
+const Profile = () => {
+  const navigate = useNavigate();
+  const onLogOutClick = () => {
+    signOut(authService);
+    navigate("/");
+  };
+  return (
+    <>
+      <button onClick={onLogOutClick}>Log Out</button>
+    </>
+  );
+};
 export default Profile;
