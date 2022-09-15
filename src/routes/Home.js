a1df2db56356d5ad70955ac2889642524b4b8068
Ukjae Lee <79234473+Gryffindor0ne@users.noreply.github.com>
2022-09-15 14:37:16 +0900

    Database setup & Create a nweet

--- src/routes/Home.js
+++ src/routes/Home.js
@@ -1,5 +1,44 @@
-import React from "react";
+import React, { useState } from "react";
+import { dbService } from "fbase";
+import { addDoc, collection } from "firebase/firestore";
 
-const Home = () => <span>Home</span>;
+const Home = () => {
+  const [nweet, setNweet] = useState("");
+
+  const onSubmit = async (event) => {
+    event.preventDefault();
+    try {
+      await addDoc(collection(dbService, "nteets"), {
+        nweet,
+        createdAt: Date.now(),
+      });
+    } catch (error) {
+      console.log(error);
+    }
+    setNweet("");
+  };
+
+  const onChange = (event) => {
+    const {
+      target: { value },
+    } = event;
+    setNweet(value);
+  };
+
+  return (
+    <div>
+      <form>
+        <input
+          onChange={onChange}
+          type="text"
+          placeholder="What's on your mind?"
+          maxLength={120}
+          value={nweet}
+        />
+        <input onClick={onSubmit} type="submit" value="Nweet" />
+      </form>
+    </div>
+  );
+};
 
 export default Home;
