60048b62969e5ec6ad54bb9b0efd872d9e5490e9
Ukjae Lee <79234473+Gryffindor0ne@users.noreply.github.com>
2022-09-15 16:52:57 +0900

    Nweet 쓰기 및 읽기 기능 구현

--- src/routes/Home.js
+++ src/routes/Home.js
@@ -1,14 +1,28 @@
-import React, { useState } from "react";
+import React, { useState, useEffect } from "react";
 import { dbService } from "fbase";
-import { addDoc, collection } from "firebase/firestore";
+import { addDoc, collection, getDocs, query } from "firebase/firestore";
 
 const Home = () => {
   const [nweet, setNweet] = useState("");
+  const [nweets, setNweets] = useState([]);
+
+  const nweetsCollectionRef = collection(dbService, "nweets");
+
+  const getNweets = async () => {
+    const q = query(nweetsCollectionRef);
+    const data = await getDocs(q);
+
+    const newData = data.docs.map((doc) => ({
+      ...doc.data(),
+      id: doc.id,
+    }));
+    setNweets(newData);
+  };
 
   const onSubmit = async (event) => {
     event.preventDefault();
     try {
-      await addDoc(collection(dbService, "nteets"), {
+      await addDoc(collection(dbService, "nweets"), {
         nweet,
         createdAt: Date.now(),
       });
@@ -25,20 +39,32 @@ const Home = () => {
     setNweet(value);
   };
 
+  useEffect(() => {
+    getNweets();
+  }, []);
+
+  console.log(nweets);
+
   return (
     <div>
-      <form>
+      <form onSubmit={onSubmit}>
         <input
+          value={nweet}
           onChange={onChange}
           type="text"
           placeholder="What's on your mind?"
           maxLength={120}
-          value={nweet}
         />
-        <input onClick={onSubmit} type="submit" value="Nweet" />
+        <input type="submit" value="Nweet" />
       </form>
+      <div>
+        {nweets.map((nweet) => (
+          <div key={nweet.id}>
+            <h4>{nweet.nweet}</h4>
+          </div>
+        ))}
+      </div>
     </div>
   );
 };
-
 export default Home;
