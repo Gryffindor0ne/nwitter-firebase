50cf41b839eb0b12a3e56ae67a554fa1889afc9c
Ukjae Lee <79234473+Gryffindor0ne@users.noreply.github.com>
2022-09-16 16:14:42 +0900

    내가 작성한 Nweets 보여지게 Profile 컴포넌트 수정

--- src/routes/Profile.js
+++ src/routes/Profile.js
@@ -1,17 +1,54 @@
-import React from "react";
+import React, { useEffect, useState } from "react";
 import { signOut } from "firebase/auth";
 import { useNavigate } from "react-router-dom";
 
-import { authService } from "fbase";
+import { authService, dbService } from "fbase";
+import {
+  collection,
+  doc,
+  getDocs,
+  query,
+  where,
+  orderBy,
+} from "firebase/firestore";
+import Nweet from "components/Nweet";
 
-const Profile = () => {
+const Profile = ({ userObj }) => {
+  const [myDocs, setMyDocs] = useState([]);
   const navigate = useNavigate();
   const onLogOutClick = () => {
     signOut(authService);
     navigate("/");
   };
+
+  const getMyNweets = async () => {
+    const q = query(
+      collection(dbService, "nweets"),
+      where("creatorId", "==", userObj.uid),
+      orderBy("createdAt", "desc")
+    );
+    const querySnapshot = await getDocs(q);
+    const myNweets = querySnapshot.docs.map((doc) => ({
+      // console.log(doc.id, " => ", doc.data());
+      ...doc.data(),
+    }));
+    setMyDocs(myNweets);
+  };
+
+  useEffect(() => {
+    getMyNweets();
+  }, []);
+
   return (
     <>
+      {myDocs &&
+        myDocs.map((doc) => (
+          <Nweet
+            key={doc.id}
+            nweetObj={doc}
+            isOwner={doc.creatorId === userObj.uid}
+          />
+        ))}
       <button onClick={onLogOutClick}>Log Out</button>
     </>
   );
